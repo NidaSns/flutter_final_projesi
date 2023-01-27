c7149501f21523cf1cea7d3aa04da371cead1164
nida_snsy <nida8581@gmail.com>
2023-01-27 23:54:22 +0300

    folders are changed.
    folders are changed.

---
+++ yemek_siparis_app/lib/ui/widgets/yemek_detay/dropdown_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/constants.dart';
+
+class DropDownButtonTasarimi extends StatelessWidget {
+  const DropDownButtonTasarimi(
+      {required this.onChanged, required this.dropdownValue, super.key});
+
+  final String dropdownValue;
+  final ValueChanged<String?>? onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton(
+      borderRadius: BorderRadius.circular(5),
+      value: dropdownValue,
+      onChanged: onChanged,
+      items: ApplicationConstants.instance!.adet
+          .map<DropdownMenuItem<String>>((int value) {
+        return DropdownMenuItem<String>(
+          value: value.toString(),
+          child: Text("Adet: $value"),
+        );
+      }).toList(),
+    );
+  }
+}
