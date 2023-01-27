b62054b4bc0a69ad706d363bccaa0f5dd9c7b3c8
nida_snsy <nida8581@gmail.com>
2023-01-27 23:48:40 +0300

    Update sepet_sayfasi.dart
    basket page designed

--- yemek_siparis_app/lib/ui/screens/sepet_sayfasi.dart
+++ yemek_siparis_app/lib/ui/screens/sepet_sayfasi.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:yemek_siparis_app/data/entity/sepettekiler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:yemek_siparis_app/ui/constants/constants.dart';
+import 'package:yemek_siparis_app/ui/screens/toplam_fiyat.dart';
 import '../cubit/sepet_cubit.dart';
-import '../widgets/dropdown_button.dart';
-import '../widgets/round_rectangle_border.dart';
+import '../widgets/sepet/add_minus_icons.dart';
+import '../widgets/sepet/list_tile.dart';
 
 class SepetSayfasi extends StatefulWidget {
   final String kullaniciAdi;
@@ -16,6 +17,8 @@ class SepetSayfasi extends StatefulWidget {
 }
 
 class _SepetSayfasiState extends State<SepetSayfasi> {
+  String toplamFiyat = "";
+
   @override
   void initState() {
     context
@@ -28,7 +31,9 @@ class _SepetSayfasiState extends State<SepetSayfasi> {
 
   @override
   Widget build(BuildContext context) {
+    context.read<SepetCubit>().sepettekiYemekleriGetir(widget.kullaniciAdi);
     double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Align(
       alignment: Alignment.topLeft,
       child: Padding(
@@ -40,87 +45,74 @@ class _SepetSayfasiState extends State<SepetSayfasi> {
               child: BlocBuilder<SepetCubit, List<Sepettekiler>>(
                 builder: (context, sepettekilerListesi) {
                   if (sepettekilerListesi.isNotEmpty) {
-                    return ListView.builder(
-                        key: widget.key,
-                        itemCount: sepettekilerListesi.length,
-                        itemBuilder: (context, index) {
-                          var sepet = sepettekilerListesi[index];
-                          String dropdownValue = sepet.sepet_siparis_adet;
-                          //String dropdownValue = calculate(sepet, fiyat);
-                          return Padding(
-                            padding: const EdgeInsets.all(15),
-                            child: Card(
-                              borderOnForeground: true,
-                              shape: RoundRectangleBorderTasarimi()
-                                  .roundedRectangleBorder(context),
-                              color: Colors.white,
-                              child: Column(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 20, right: 20),
+                    toplamFiyat = ToplamFiyat(sepet: sepettekilerListesi)
+                        .calculateTotalPrice();
+                    return Column(
+                      children: [
+                        SizedBox(
+                          height: height / 1.5,
+                          child: ListView.builder(
+                              key: widget.key,
+                              itemCount: sepettekilerListesi.length,
+                              itemBuilder: (context, index) {
+                                var sepet = sepettekilerListesi[index];
+                                String dropdownValue = sepet.sepet_siparis_adet;
+                                return Padding(
+                                  padding: const EdgeInsets.all(15),
+                                  child: Card(
                                     child: Row(
                                       children: [
-                                        Align(
-                                          alignment: Alignment.topLeft,
+                                        Flexible(
+                                          flex: 1,
+                                          child: Image.network(
+                                              "http://kasimadalan.pe.hu/yemekler/resimler/${sepet.sepet_yemek_resim_adi}"),
+                                        ),
+                                        Flexible(
+                                          flex: 1,
                                           child: Column(
                                             children: [
-                                              SizedBox(
-                                                width: width / 4,
-                                                child: Image.network(
-                                                    "http://kasimadalan.pe.hu/yemekler/resimler/${sepet.sepet_yemek_resim_adi}"),
-                                              ),
-                                              Text(
-                                                sepet.sepet_yemek_adi,
-                                                style: const TextStyle(
-                                                    fontWeight: FontWeight.bold,
-                                                    fontSize: 25),
+                                              ListTileBasket(sepet: sepet),
+                                              AddMinusIcons(
+                                                dropdownValue: dropdownValue,
+                                                sepet: sepet,
                                               ),
                                             ],
                                           ),
                                         ),
-                                        const Spacer(),
-                                        Column(
-                                          children: [
-                                            DropDownButtonTasarimi(
-                                                onChanged: (String? value) {
-                                                  setState(() {
-                                                    dropdownValue = value!;
-                                                  });
-                                                },
-                                                dropdownValue: dropdownValue),
-                                            Text(
-                                              "Birim Fiyat: ${sepet.sepet_yemek_fiyat}₺",
-                                              style: const TextStyle(
-                                                  fontSize: 17,
-                                                  fontWeight: FontWeight.bold),
-                                            )
-                                          ],
-                                        ),
                                       ],
                                     ),
                                   ),
-                                  const SizedBox(
-                                    height: 20,
-                                  ),
-                                  SizedBox(
-                                    width: width / 2.5,
-                                    child: ElevatedButton(
-                                      onPressed: () {
-                                        context
-                                            .read<SepetCubit>()
-                                            .sepettenYemekSil(
-                                                int.parse(sepet.sepet_yemek_id),
-                                                sepet.kullanici_adi);
-                                      },
-                                      child: const Text("Sil"),
+                                );
+                              }),
+                        ),
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: Column(
+                            children: [
+                              Text(
+                                "Toplam Fiyat: $toplamFiyat ₺",
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(
+                                width: width / 2,
+                                child: ElevatedButton.icon(
+                                  icon: const Icon(Icons.check_circle_rounded),
+                                  onPressed: () {},
+                                  label: const Text("Sepeti Onayla"),
+                                  style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(32.0),
                                     ),
                                   ),
-                                ],
+                                ),
                               ),
-                            ),
-                          );
-                        });
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
                   } else {
                     return Center(
                       child: ApplicationConstants.instance!.sepetData == ""
@@ -131,46 +123,9 @@ class _SepetSayfasiState extends State<SepetSayfasi> {
                 },
               ),
             ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Column(
-                children: [
-                  // Text(
-                  //   "Toplam Fiyat: ${ApplicationConstants.toplamFiyat} ₺",
-                  //   style: const TextStyle(
-                  //     fontWeight: FontWeight.bold,
-                  //   ),
-                  // ),
-                  SizedBox(
-                    width: width / 2,
-                    child: ElevatedButton.icon(
-                      icon: const Icon(Icons.check_circle_rounded),
-                      onPressed: () {
-                        ///toDo
-                      },
-                      label: const Text("Sepeti Onayla"),
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(32.0),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
           ],
         ),
       ),
     );
   }
-
-  // String calculate(Sepettekiler sepet, double fiyat) {
-  //   String dropdownValue = sepet.sepet_siparis_adet;
-  //   fiyat = double.parse(sepet.sepet_yemek_fiyat);
-  //   int urunAdet = int.parse(dropdownValue);
-  //   double birimToplam = urunAdet * fiyat;
-  //   ApplicationConstants.toplamFiyat += birimToplam;
-  //   return dropdownValue;
-  // }
 }
